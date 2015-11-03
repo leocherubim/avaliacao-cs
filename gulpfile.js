70363f3def3a883f24c035c60d3eb782d171d5b8
vagrant <vagrant@precise32.(none)>
2015-11-03 17:44:36 +0000

    Servico REST testado

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,20 @@
 var elixir = require('laravel-elixir');
 
 /*
+ * Configuracao de caminhos dos arquivos
+ */
+var config = {
+	assets_path:'./resources/assets',
+	build_path:'./public/build'
+};
+
+/*
+ * Configuracao de caminhos dos arquivos dentro
+ * da pasta bower_components
+ *
+config.bower_path = config.assets_path + '/../bower_components';
+
+/*
  |--------------------------------------------------------------------------
  | Elixir Asset Management
  |--------------------------------------------------------------------------
